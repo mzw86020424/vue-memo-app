745ab423451ad04fe5bffed78ccce0956843968d
mzw86020424 <mzw86020424@gmail.com>
2021-02-20 12:09:07 +0900

    create home route, controller

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,45 +1,43 @@
 /* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
+// Run this example by adding <%= javascript_pack_tag '_vue' %> (and
+// <%= stylesheet_pack_tag '_vue' %> if you have styles in your component)
 // to the head of your layout file,
 // like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
+// All it does is render <div> Vue</div> at the bottom of the page.
 
-import Vue from 'vue'
-import App from '../app.vue'
+import Vue from "vue";
+import App from "../app.vue";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
+    render: (h) => h(App),
+  }).$mount();
+  document.body.appendChild(app.$el);
 
-  console.log(app)
-})
-
+  console.log(app);
+});
 
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
 // need to always use single file components.
 // To be able to target elements in your existing html/erb templates,
 // comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
+// Add <%= javascript_pack_tag '_vue' %> to your layout
 // Then add this markup to your html template:
 //
-// <div id='hello'>
+// <div id=''>
 //   {{message}}
 //   <app></app>
 // </div>
-
 
 // import Vue from 'vue/dist/vue.esm'
 // import App from '../app.vue'
 //
 // document.addEventListener('DOMContentLoaded', () => {
 //   const app = new Vue({
-//     el: '#hello',
+//     el: '#',
 //     data: {
-//       message: "Can you say hello?"
+//       message: "Can you say ?"
 //     },
 //     components: { App }
 //   })
@@ -61,10 +59,10 @@ document.addEventListener('DOMContentLoaded', () => {
 //
 // document.addEventListener('turbolinks:load', () => {
 //   const app = new Vue({
-//     el: '#hello',
+//     el: '#',
 //     data: () => {
 //       return {
-//         message: "Can you say hello?"
+//         message: "Can you say ?"
 //       }
 //     },
 //     components: { App }
